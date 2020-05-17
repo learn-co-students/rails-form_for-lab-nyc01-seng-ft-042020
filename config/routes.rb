c284039c1a52bef6e6ea3417519ffefb4473ad25
Jessica <github email address>
2020-05-17 10:28:22 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :students, only: [:create, :new, :show, :edit, :update]
+  resources :school_classes, only: [:create, :new, :show, :edit, :update]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "...destroying School Classes"
+SchoolClass.destroy_all
+
+puts "...destroying Student Data"
+Student.destroy_all
+
+puts "...creating School Classes"
+10.times do
+    SchoolClass.create(title: Faker::Educator.course_name, room_number: rand(1001..1005))
+end
+
+puts "...creating Student Data"
+25.times do 
+    Student.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+end
